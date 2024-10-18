0053e238278f0e4081e71a9b6acb856408e017c6
Michael <200627081@student.georgianc.on.ca>
2024-10-17 20:37:28 -0400

    Added Movement Script onto Player

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction = Vector2.right;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            direction = Vector2.left;
+        }
+        float dt = Time.deltaTime;
+        float speed = 3.0f;
+        Vector3 change = direction * speed * dt;
+        transform.position = transform.position + change;
+    }
+}
