45ce50a5b07b2a2c685e10527789a85e14f2dc06
Michael <200627081@student.georgianc.on.ca>
2024-12-12 16:56:43 -0500

    Limited to 1 jump

--- Assets/Scripts/Movement1.cs
+++ Assets/Scripts/Movement1.cs
@@ -8,26 +8,40 @@ public class Movement1 : MonoBehaviour
     public float speed = 5.0f;
     public float jumpForce = 10.0f;
 
-    private Vector2 joystickMovment;
+    private Vector2 joystickMovement;
     private Rigidbody2D rBody;
+    private bool isGrounded = true;
+
     void Start()
     {
-       rBody = GetComponent<Rigidbody2D>();
+        rBody = GetComponent<Rigidbody2D>();
     }
 
+    [System.Obsolete]
     private void Update()
     {
-        rBody.linearVelocityX = joystickMovment.x * speed;
+        rBody.velocity = new Vector2(joystickMovement.x * speed, rBody.velocity.y);
     }
+
     public void OnMove(InputAction.CallbackContext valueFromAction)
     {
-        joystickMovment = valueFromAction.ReadValue<Vector2>();
-        
+        joystickMovement = valueFromAction.ReadValue<Vector2>();
     }
+
     public void OnJump(InputAction.CallbackContext ctx)
     {
-
-        rBody.AddForceY(jumpForce, ForceMode2D.Impulse);
+        if (ctx.performed && isGrounded)
+        {
+            rBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
     }
-   
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts[0].normal.y > 0.5f)
+        {
+            isGrounded = true;
+        }
+    }
 }
