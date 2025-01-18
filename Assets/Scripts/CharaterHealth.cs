a76eccddf93c7f9f777ad4f4076b1b054dfced63
Damien-arch <200602230@student.georgianc.on.ca>
2025-01-18 13:41:37 -0500

    Updated, added tag to slime
    trying to do an on enter get tag method for collision except forgot how get tag works

--- Assets/Scripts/CharaterHealth.cs
+++ Assets/Scripts/CharaterHealth.cs
@@ -7,14 +7,28 @@ public class CharaterHealth : MonoBehaviour
     float[] numbers = { 0f, 1f, 2f, 3f, 4f, 5f };
     float maxHealth = 5f;
     float minHealth = 0f;
+    float currenthealth = 5f;
 
     void Start()
     {
-        
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if ()
+            {
+                
+            }
+        }
     }
 
     void Update()
     {
-        
+        //if player ridgid body collides with enemy ridgid body, player takes dam and losses a heart
+
+        //if player health is down to zero player dies
     }
 }
