893932e37fec32a52fcd6dfc3e666c35b7ab5023
Damien-arch <200602230@student.georgianc.on.ca>
2025-01-18 13:03:53 -0500

    Update to character health code

--- Assets/Scripts/CharaterHealth.cs
+++ Assets/Scripts/CharaterHealth.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 
 public class CharaterHealth : MonoBehaviour
 {
-   
+    GameObject Player;
+
+    float[] numbers = { 0f, 1f, 2f, 3f, 4f, 5f };
+    float maxHealth = 5f;
+    float minHealth = 0f;
+
     void Start()
     {
         
