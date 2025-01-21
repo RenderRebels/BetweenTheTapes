3c2c7d70766af07236e0ccf1a7baf3f538f9564e
Damien-arch <200602230@student.georgianc.on.ca>
2025-01-21 17:13:16 -0500

    Update to Character Health code
    Hopefully a functional code

--- Assets/Scripts/CharaterHealth.cs
+++ Assets/Scripts/CharaterHealth.cs
@@ -5,25 +5,26 @@ public class CharaterHealth : MonoBehaviour
     GameObject Player;
 
     float[] numbers = { 0f, 1f, 2f, 3f, 4f, 5f };
-    float maxHealth = 5f;
-    float minHealth = 0f;
-    float currentHealth = 5f;
-    float damage = -1f;
 
     void Start()
     {
-        //float maxHealth = 5f;
-        //float minHealth = 0f;
-        //float currentHealth = 5f;
-        //float damage = -1f;
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
+        int maxHealth = 5;
+        int minHealth = 0;
+        int damage = -1;
+        int currentHealth = maxHealth + damage;
+
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            Debug.Log("health"  );
+            Debug.Log("health" + currentHealth);
+        }
+        if (currentHealth <= minHealth);
+        {
+            Destroy(Player);
         }
     }
 
