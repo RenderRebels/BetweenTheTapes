49d08a10824ad002c763fd91cd765d9929938099
Gage-Atlas <Gagebreedon@gmail.com>
2025-01-30 17:50:47 -0500

    fIX

--- Assets/Scripts/Movement1.cs
+++ Assets/Scripts/Movement1.cs
@@ -5,58 +5,44 @@ using UnityEngine.InputSystem;
 
 public class Movement1 : MonoBehaviour
 {
-    float horizontalInput;
-    float moveSpeed = 5f;
-    bool isFacingRight = false;
-    float jumpPower = 10.0f;
-    bool isGrounded = false;
+    public float speed = 5.0f;
+    public float jumpForce = 10.0f;
 
-    Rigidbody2D rb;
-    Animator animator;
+    private Vector2 joystickMovement;
+    private Rigidbody2D rBody;
+    private bool isGrounded = true;
 
-    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        rBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    [System.Obsolete]
+    private void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+        rBody.velocity = new Vector2(joystickMovement.x * speed, rBody.velocity.y);
+    }
 
-        FlipSprite();
+    public void OnMove(InputAction.CallbackContext valueFromAction)
+    {
+        joystickMovement = valueFromAction.ReadValue<Vector2>();
+    }
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed && isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            rBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
-            animator.SetBool("isJumping", !isGrounded);
         }
     }
 
-    private void FixedUpdate()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
-        animator.SetFloat("xVelocity", Math.Abs(rb.velocity.x));
-        animator.SetFloat("yVelocity", rb.velocity.y);
-    }
-
-    void FlipSprite()
-    {
-        if(isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        if (collision.contacts[0].normal.y > 0.5f)
         {
-            isFacingRight = !isFacingRight;
-            Vector3 ls = transform.localScale;
-            ls.x *= -1f;
-            transform.localScale = ls;
+            isGrounded = true;
         }
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        isGrounded = true;
-        animator.SetBool("isJumping", !isGrounded);
-    }
+    
 }
